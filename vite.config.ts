e2476b47de172375a516235fdf3a95db25e53881
JAIPRAKASH-07 <your.sjaiprakash682@gmail.com>
2025-11-21 09:39:19 +0530

    Update Vite React project

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import path from "path"
 
 export default defineConfig({
   plugins: [react()],
+   build: {
+    chunkSizeWarningLimit: 2000, // increase limit to 2 MB if needed
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
